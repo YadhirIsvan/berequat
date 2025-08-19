d5cb99d442aa160dbeda8aa62a145bd704488f87
Angel Yamir <innovogen375@gmail.com>
2025-08-19 12:50:57 -0600

    asdfasdf

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -42,10 +42,9 @@ export const Contact = (props) => {
           <div className="col-md-8">
             <div className="row">
               <div className="section-title">
-                <h2>Get In Touch</h2>
+                <h2>Ponte en contacto</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  Por favor, rellene el formulario a continuación para enviarnos un correo electrónico y nos pondremos en contacto con usted lo antes posible.
                 </p>
               </div>
               <form name="sentMessage" validate onSubmit={handleSubmit}>
@@ -100,7 +99,7 @@ export const Contact = (props) => {
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
             <div className="contact-item">
-              <h3>Contact Info</h3>
+              <h3>Informacion de contacto</h3>
               <p>
                 <span>
                   <i className="fa fa-map-marker"></i> Address
